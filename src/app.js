d6915d699b7ce1b3a9784bee17c68d9b632b4022
tmdwnsasa <tmdwns_as@naver.com>
2024-05-23 15:02:55 +0900

    ADD 에러 핸들링 미들웨어 제작

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import errorHandlingMiddleware from "./middlewares/error-handling.middleware.js";
 import UsersRouter from "./routes/users.router.js";
 import ItemsRouter from "./routes/items.router.js";
 
@@ -11,6 +12,8 @@ app.use("/api", [UsersRouter, ItemsRouter]);
 app.get("/", async (req, res) => {
   return res.status(200).json({ message: "어 그래 형이야" });
 });
+
+app.use(errorHandlingMiddleware);
 
 app.listen(PORT, () => {
   console.log(PORT, "포트로 서버가 열렸다~");
