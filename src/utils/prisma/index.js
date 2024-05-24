89a059371092a5bec6d0e2ce258108c5079afbfd
tmdwnsasa <tmdwns_as@naver.com>
2024-05-24 14:38:11 +0900

    FIX 프리티어 수정, user 스키마, character스키마 수정
    index import위치 수정

--- src/utils/prisma/index.js
+++ src/utils/prisma/index.js
@@ -1,5 +1,5 @@
-import { PrismaClient as GamePrismaClient } from "../../../prisma/gameDataClient/default.js";
 import { PrismaClient as UserPrismaClient } from "../../../prisma/userDataClient/default.js";
+import { PrismaClient as GamePrismaClient } from "../../../prisma/gameDataClient/default.js";
 
 export const gamePrisma = new GamePrismaClient({
   log: ["query", "info", "warn", "error"],
